92a6d77132d26f5f8cc1be8e12faf7b5d6971712
huangchangguo <huangchangguo@szprize.com>
2018-05-16 11:48:58 +0800

    test commit

--- app/src/main/java/com/prize/dagger2_demo/ScrollingActivity.java
+++ app/src/main/java/com/prize/dagger2_demo/ScrollingActivity.java
@@ -59,7 +59,7 @@ public class ScrollingActivity extends AppCompatActivity {
                 int currentValue = (Integer) animator.getAnimatedValue();
 
                 // 获得每次变化后的属性值
-                //System.out.println(currentValue);
+                System.out.println(currentValue);
                 // 输出每次变化后的属性值进行查看
                 ViewGroup.LayoutParams params = view.getLayoutParams();
                 params.width = currentValue;
